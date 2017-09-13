42b4e5ebad63c8938b39d9a4f8479d6721b12679
madansakilam1425 <31914962+madansakilam1425@users.noreply.github.com>
2017-09-13 15:00:12 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -257,7 +257,9 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public int getCurrentAgeByDate(int indexOfStudent) {
-		
+		if(indexOfStudent == 0){
+			throw new IllegalArgumentException("Null");
+		}
 		Date now = new Date();
            return this.students[indexOfStudent].getBirthDate().getYear() - now.getYear();
 	}
